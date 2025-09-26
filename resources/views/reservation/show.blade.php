8a3e87af229ec046a36b70f2b41a92867464f36b
xyzidk <csokiscsigakakaos@gmail.com>
2025-09-26 14:02:10 +0200

    added missing details to reservation show view

--- resources/views/reservation/show.blade.php
+++ resources/views/reservation/show.blade.php
@@ -1,17 +1,17 @@
 @extends('layout')
 
 @section('content')
-
-<div style="margin-left: 35%">
-<h2>Reservation details</h2>
-Car ID: {{$reservation->car_id}}<br>
-Phone Number: {{ $reservation->phone_number }}<br>
-Email: {{ $reservation->email }}<br>
-Address: {{ $reservation->address }}<br>
-Start Date: {{ $reservation->date_from }}<br>
-End Date: {{ $reservation->date_to }}<br>
-Days: {{ $reservation->days }}<br>
-Total Price: {{ $reservation->total_price }}<br>
-</div>
-<a href="{{ route('reservation.index') }}" style="display:block; text-align:center; margin: 24px 0 0 0;"> Back</a>
+    <div style="margin-left: 35%">
+        <h2>Reservation details</h2>
+        Car ID: {{ $reservation->car_id }}<br>
+        Name: {{ $reservation->name }}<br>
+        Phone Number: {{ $reservation->phone }}<br>
+        Email: {{ $reservation->email }}<br>
+        Address: {{ $reservation->address }}<br>
+        Start Date: {{ $reservation->date_from }}<br>
+        End Date: {{ $reservation->date_to }}<br>
+        Days: {{ $reservation->days }}<br>
+        Total Price: {{ $reservation->total_price }}<br>
+    </div>
+    <a href="{{ route('reservation.index') }}" style="display:block; text-align:center; margin: 24px 0 0 0;"> Back</a>
 @endsection
