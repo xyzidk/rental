ff4e04fb6b7584c629d5a399eff67d618fd34b45
xyzidk <csokiscsigakakaos@gmail.com>
2025-09-26 09:32:15 +0200

    filtered cars by reservation period

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -2,33 +2,32 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
 use App\Models\Car;
 use App\Models\Reservation;
-
+use Illuminate\Http\Request;
 
 class MainController extends Controller
 {
     public function listCars(Request $request)
-{
-    $request->validate([
-        'date_from' => 'required|date',
-        'date_to' => 'required|date|after_or_equal:date_from',
-    ]);
+    {
+        $request->validate([
+            'date_from' => 'required|date',
+            'date_to' => 'required|date|after_or_equal:date_from',
+        ]);
 
-    $reservedCarIds = Reservation::where(function($q) use ($request) {
-        $q->where('date_from', '<=', $request->date_to)
-          ->where('date_to', '>=', $request->date_from);
-    })->pluck('car_id');
+        $reservedCarIds = Reservation::where(function ($q) use ($request) {
+            $q->where('date_from', '<=', $request->date_to)
+                ->where('date_to', '>=', $request->date_from);
+        })->pluck('car_id');
 
-    $cars = Car::whereNotIn('id', $reservedCarIds)
-        ->where('status', 'not_reserved')
-        ->get();
+        $cars = Car::whereNotIn('id', $reservedCarIds)
+            ->where('status', '!=', 'deactivated')
+            ->get();
 
-    return view('index', [
-        'cars' => $cars,
-        'date_from' => $request->date_from,
-        'date_to' => $request->date_to,
-    ]);
-}
+        return view('index', [
+            'cars' => $cars,
+            'date_from' => $request->date_from,
+            'date_to' => $request->date_to,
+        ]);
+    }
 }
